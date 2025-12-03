f45a2df41c366608c4ed7edb242ea2812714c719
Debuging-JunSeoPark <pjseo1313@daum.net>
2025-12-04 01:06:53 +0900

    fix: include application.yml

--- src/main/java/com/example/WEEK04/config/WebConfig.java
+++ src/main/java/com/example/WEEK04/config/WebConfig.java
@@ -1,6 +1,5 @@
 package com.example.WEEK04.config;
 
-
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -10,11 +9,18 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // 모든 엔드포인트 허용
-                .allowedOrigins("http://localhost:5174", "http://localhost:5173", "http://localhost:4173", "http://localhost:4174","http://172.20.4.236:5173") // 프론트엔드 주소
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS","PATCH")
-                .allowedHeaders("*") // 모든 헤더 허용
-                .allowCredentials(true) // 쿠키/세션 포함 허용
-                .maxAge(3600); // preflight 캐시 (1시간)
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:5174",
+                        "http://localhost:5173",
+                        "http://localhost:4173",
+                        "http://localhost:4174",
+                        "http://172.20.4.236:5173",
+                        "https://logo-rithm.vercel.app"   // ✅ 프론트 배포 URL 추가!
+                )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }
